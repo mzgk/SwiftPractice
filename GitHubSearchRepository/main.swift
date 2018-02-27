5b04b5fddbd156aabaf8806b8209e4555a656f89
mzgk <mzgkworks@gmail.com>
2018-02-27 09:30:58 +0900

    Initial Commit

---
+++ GitHubSearchRepository/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GitHubSearchRepository
+//
+//  Created by mzgk on 2018/02/27.
+//  Copyright © 2018年 mzgk. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
