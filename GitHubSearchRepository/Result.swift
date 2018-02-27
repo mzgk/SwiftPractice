5f7f0847a47c58e567062dd2b396bdbfe26b8958
mzgk <mzgkworks@gmail.com>
2018-02-27 15:23:09 +0900

    add: APIクライアント

--- GitHubSearchRepository/Result.swift
+++ GitHubSearchRepository/Result.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+
+enum Result<T, Error: Swift.Error> {
+    case success(T)
+    case failure(Error)
+
+    init(value: T) {
+        self = .success(value)
+    }
+
+    init(error: Error) {
+        self = .failure(error)
+    }
+}
